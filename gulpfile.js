689f7cc3683f5ccbcb644d88263b942425af9d6d
TbcProjects <daniel@thebikeclub.co.uk>
2022-07-07 14:58:33 +0100

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
-const themekit = require("@shopify/themekit");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const themeKit = require("@shopify/themekit");
 
 gulp.task("sass", function () {
-  return gulp.src("styles/theme.scss").pipe(sass()).pipe(gulp.dest("assets"));
+  return gulp
+    .src("styles/theme.scss")
+    .pipe(sass())
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(gulp.dest("assets"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("styles/**/*.scss", gulp.series("sass"));
 });
